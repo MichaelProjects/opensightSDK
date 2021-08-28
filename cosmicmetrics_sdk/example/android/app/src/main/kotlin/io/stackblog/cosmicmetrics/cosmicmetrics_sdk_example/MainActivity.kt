0787f4cf9ffb42cd1338dc2294f6d8516995eac3
MichaelProjects <michael.lichtenecker@outlook.com>
2021-08-29 00:38:40 +0200

    started working on flutter sdk

--- cosmicmetrics_sdk/example/android/app/src/main/kotlin/io/stackblog/cosmicmetrics/cosmicmetrics_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.stackblog.cosmicmetrics.cosmicmetrics_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- cosmicmetrics_sdk/lib/cosmicmetrics_sdk.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-class AnalyticsSdk {
-  final String analyticsUrl = "";
-  final String applicationToken = "";
-  final String sessionId = "";
-
-  static Future<void> initializeApp() async {}
-}
