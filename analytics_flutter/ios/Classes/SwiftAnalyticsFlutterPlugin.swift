a51985a7996b8bd567f655e79c25f9bf2a7a3612
MichaelProjects <michael.lichtenecker@outlook.com>
2021-09-13 23:47:57 +0200

    update

--- analytics_flutter/ios/Classes/SwiftAnalyticsFlutterPlugin.swift
+++ analytics_flutter/ios/Classes/SwiftAnalyticsFlutterPlugin.swift
@@ -9,37 +9,42 @@ public class SwiftAnalyticsFlutterPlugin: NSObject, FlutterPlugin {
   }
 
     public func handle(_ call: FlutterMethodChannel, result: @escaping FlutterResult){
-        switch(call.method) {
+        switch call.method {
         case "getPlatformVersion":
-            result("IOS" + UIDevice.current.systemVersion)
+            result(String(format: "IOS %1$@", UIDevice.current.systemVersion))
+            break;
         case "dispalySize":
             result(getDisplaySize())
+            break;
         case "getOpensightConfig":
-            result(readConf())
+            result(readJSONFromFile(fileName: "opensight_service.json"))
+            break;
         default:
             result("Not Implemented")
         }
     }
+    
+    public func readJSONFromFile(fileName: String) -> Any? {
+      var json: Any?
+      if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
+          do {
+              let fileUrl = URL(fileURLWithPath: path)
+              // Getting data from JSON file using the file URL
+              let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+              json = try? JSONSerialization.jsonObject(with: data)
+          } catch {
+              // Handle error here
+          }
+      }
+      return json
+  }
 
-}
-
-public func readConf(){
-    if let path = NSBundle.mainBundle().pathForResource("opensight_service", ofType: "json")
-    {
-        if let jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)
-        {
-            if let jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSDictionary
-            {
-                return jsonResult
-            }
-         }
-    }
-}
-
-public func getDisplaySize(){
+public func getDisplaySize() -> String {
     let screenSize: CGRect = UIScreen.main.bounds
     let screenWidth = screenSize.width
     let screenHeight = screenSize.height
-    return screenWidth + "x" + screenHeight
+    return String(format: "%2$@ + %1$@", screenWidth, screenHeight)
 }
 
+
+}
