3b4545e101051e01c7001b7f8a0813467b89574d
MichaelProjects <michael.lichtenecker@outlook.com>
2021-09-15 16:13:04 +0200

    tryed to fix ios integartion

--- analytics_flutter/ios/Classes/SwiftAnalyticsFlutterPlugin.swift
+++ analytics_flutter/ios/Classes/SwiftAnalyticsFlutterPlugin.swift
@@ -2,49 +2,49 @@ import Flutter
 import UIKit
 
 public class SwiftAnalyticsFlutterPlugin: NSObject, FlutterPlugin {
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "io.opensight", binaryMessenger: registrar.messenger())
     let instance = SwiftAnalyticsFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-    public func handle(_ call: FlutterMethodChannel, result: @escaping FlutterResult){
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "getPlatformVersion":
             result(String(format: "IOS %1$@", UIDevice.current.systemVersion))
             break;
-        case "dispalySize":
+        case "displaysize":
             result(getDisplaySize())
             break;
         case "getOpensightConfig":
-            result(readJSONFromFile(fileName: "opensight_service.json"))
+            result(readConf())
             break;
         default:
             result("Not Implemented")
         }
     }
     
-    public func readJSONFromFile(fileName: String) -> Any? {
-      var json: Any?
-      if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
-          do {
-              let fileUrl = URL(fileURLWithPath: path)
-              // Getting data from JSON file using the file URL
-              let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
-              json = try? JSONSerialization.jsonObject(with: data)
-          } catch {
-              // Handle error here
-          }
-      }
-      return json
-  }
+    private func readConf() -> Any {
+        var conf = ""
+        iif let filepath = Bundle.main.path(forResource: "opensight_conf", ofType: "json") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                print(contents)
+            } catch {
+                print("Error could not load file")
+            }
+        } else {
+            print("Could not find opensight_service.json
+        }
+        return conf
+    }
 
-public func getDisplaySize() -> String {
-    let screenSize: CGRect = UIScreen.main.bounds
-    let screenWidth = screenSize.width
-    let screenHeight = screenSize.height
-    return String(format: "%2$@ + %1$@", screenWidth, screenHeight)
-}
-
-
+    private func getDisplaySize() -> String {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        return String(format: "%2$@ + %1$@", screenWidth, screenHeight)
+    }
+    
 }
