fa485fe2897e79f9521c00e3a0c17591ec453328
MichaelProjects <michael.lichtenecker@outlook.com>
2021-10-17 19:31:43 +0200

    added github action and codecov

--- opensight_analytics/test/collector_test.dart
+++ opensight_analytics/test/collector_test.dart
@@ -4,11 +4,11 @@ import 'package:opensight_analytics/src/collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  setUp(() async {
-    SharedPreferences.setMockInitialValues({"io.opensight/isNewUser": "true"});
-    TestDefaultBinaryMessengerBinding.instance!.defaultBinaryMessenger
-        .setMockMethodCallHandler(MethodChannel("io.opensight"),
-            (message) async {
+  testWidgets("Test collector", (widgetTester) async {
+    SharedPreferences.setMockInitialValues({"io.opensight/isNewUser": true});
+    MethodChannel channel = const MethodChannel("io.opensight_analytics");
+    widgetTester.binding.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (message) async {
       switch (message.method) {
         case "getPlatformVersion":
           return "30";
@@ -26,30 +26,11 @@ void main() {
           return null;
       }
     });
-  });
-  test("Test collector", () async {
+
     Collection collection = await Collection.collect();
     assert(collection.country == "de");
     assert(collection.deviceType == "iPhone17");
     assert(collection.deviceSize == "400x400");
   });
+  test("Test collector", () async {});
 }
-
-/*
-    switch (call.method) {
-      case "getPlatformVersion":
-        return "30";
-      case "displaysize":
-        return "400x400";
-      case "getOpensightConfig":
-        return "";
-      case "getLangCode":
-        return "de";
-      case "getAppVersion":
-        return "1.0";
-      case "getDeviceType":
-        return "iPhone17";
-      default:
-        return null;
-    }
-*/
